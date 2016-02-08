4e4488c82ba5816903964e41e08837493b9bfac9
Marten Seemann <martenseemann@gmail.com>
2016-02-08 15:40:36 +0700

    speed up build process

--- build/build.sh
+++ build/build.sh
@@ -10,6 +10,9 @@ echo "Retrieving code from GIT repo..."
 git clone --quiet ../.. .
 
 echo "Installing bower dependencies..."
+# speed up build process by copying the dev files
+mkdir bower_components
+cp -r $DIR/../bower_components/jssor-slider bower_components
 bower install
 
 rm -rf .git .gitignore bower.json .bowerrc
